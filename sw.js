abff254f1c1e1f87902358e208a03b02e066462f
DrQuack32 <72686602+DrQuack32@users.noreply.github.com>
2026-08-09 23:59:15 +1200

    Update service worker cache name

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'assessor-timer-v0.4-beta';
+const CACHE = 'football-match-timer-v0.4-beta';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))); self.clients.claim(); });
